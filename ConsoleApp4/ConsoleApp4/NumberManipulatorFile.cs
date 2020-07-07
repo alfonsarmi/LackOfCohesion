5c040440989ddcbfa0b4d55a056f89b2768e7dce
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-07-07 15:01:44 +0200

    changes

--- ConsoleApp4/ConsoleApp4/NumberManipulatorFile.cs
+++ ConsoleApp4/ConsoleApp4/NumberManipulatorFile.cs
@@ -7,26 +7,53 @@ using System.Threading.Tasks;
 namespace ConsoleApp4
 {
 
-    public class NumberManipulatorFile
-    {
-        private int _number;
+	public class NumberManipulatorFile
+	{
+		public NumberManipulatorFile()
+		{
 
-        public int NumberValue => _number;
 
-        public void AddOne() => _number++;
+		}
 
-        public void SubtractOne() => _number--;
-    }
+		private int _number;
 
-    public class NonCohesiveNumberManipulator
-    {
-        private int _firstNumber;
-        private int _secondNumber;
-        private int _thirdNumber;
+		public int NumberValue => _number;
 
-        public void IncrementFirst() => _firstNumber++;
-        public void IncrementSecond() => _secondNumber++;
-        public void IncrementThird() => _thirdNumber++;
-    }
+		public void AddOne() => _number++;
+
+		public void SubtractOne() => _number--;
+
+		public int testproperty
+		{
+			get
+			{
+				return _number;
+			}
+			set { }
+		}
+
+		public void Test1()
+		{
+			int number = 0;
+			_number = 0;
+		}
+
+		private void Test2(int test)
+		{
+			int _number = 0;
+			test = 0;
+		}
+	}
+
+	public class NonCohesiveNumberManipulator
+	{
+		private int _firstNumber;
+		private int _secondNumber;
+		private int _thirdNumber;
+
+		public void IncrementFirst() => _firstNumber++;
+		public void IncrementSecond() => _secondNumber++;
+		public void IncrementThird() => _thirdNumber++;
+	}
 
 }
